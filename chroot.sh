0b13f183a4fded14f41d30589cb64cf72aaacc3c
disque-monde <disquemonde@riseup.net>
2017-11-04 21:34:36 +0100

    modifié :         chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -46,8 +46,36 @@ useradd -g users -m $USER1
 passwd $USER1
 
 post(){
-  read -p 'Voulez vous installer Xorg ? (Y/N)' REP
+  read -p 'Voulez vous installer Xorg ? (Y/N): ' REP
   if [ $REP = Y ]; then
     pacman -S xorg
-    read -p 'Voulez vous '
+  fi
+  read -p 'Voulez vous installer un gestionair de connexion (Y/N): ' REP
+  if [ $REP = Y ]; then
+    echo "1) gdm"
+    echo "2) lightdm"
+    echo "3) sddm"
+    read -p 'Votre choix: ' REP
+    case $REP in
+      1) pacman -S gdm ;;
+      2) pacman -S lightmd ;;
+      3) pacman -S sddm ;;
+    esac
+  fi
+  read -p "Voulez vous installer un environnement graphique (Y/N)" REP
+    if [ $REP = Y ]; then
+      echo "1) gnome"
+      echo "2) KDE"
+      echo "3) XFCE"
+      read -p "Choix de l'environnement graphique: " REP
+      case $REP in
+        1) pacman -S gnome;;
+        2) pacman -S plasma-desktop ;;
+        3) pacman -S xfce ;;
+      esac
+    fi
+  echo ":: Installation des utilitaires usuels"
+  pacman -S firefox firefox-i18n-fr libreoffice-still vlc zsh \
+            ruby perl transmission-gtk virtualbox zip unzip rar 7zip \
+            atom filezilla
 }
